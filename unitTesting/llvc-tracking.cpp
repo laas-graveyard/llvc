f1580cee1f3ddbb8f25506fb4dc6b4cc15599b33
Thomas Moulard <thomas.moulard@gmail.com>
2010-08-17 15:45:00 +0900

    Make llvc-tracking test more verbose and catch throwed C strings (const char*).

--- unitTesting/llvc-tracking.cpp
+++ unitTesting/llvc-tracking.cpp
@@ -17,62 +17,77 @@
 #include <llvc/tools/indent.hh>
 
 #include <visp/vpDisplayX.h>
-   
+
 using namespace trackingClient;
 
-int main ()  
+int main ()
 {
   vpHomogeneousMatrix cMo;
 
   int Verbose = 0;
 
-  ODEBUG3("\n1. Construct Grabber\n");
-  // Start with TRIGGER_MODE
-  boost::shared_ptr<ActionGrab> clientGrab(new ActionGrab(true));
-  assert (clientGrab);
-  
-  ODEBUG3("\n2. Initialize Grabber\n");
-  clientGrab->Initialize();
-  if (Verbose>2)
-    std::cout << *clientGrab << std::endl;
-  
-  ODEBUG3("\n3. Execute Grabber\n");
-  clientGrab->ExecuteAction();
-    
-  ODEBUG3("\n4. Construct Display\n");
-  ActionDisplayMbt display(clientGrab, "ElectricWallFar", "default");
-  if (Verbose>2)
-    std::cout << *clientGrab << std::endl;
-  
-  ODEBUG3("\n4. Initialize Display\n");
-  display.Initialize();
-  
-  // Switch to FLOW
-  clientGrab->setTriggerMode(false);
-
-  ODEBUG3("\n5. Execute Display\n");
-  display.ExecuteAction();
-  
-  ODEBUG3("\n6. Print all\n");
-  if (Verbose>2)
-    std::cout << *clientGrab << iendl
-	      << display << iendl;
-  
-  ODEBUG3("\n7. LOOP\n");
-  
-  unsigned int nbIter = 3000000;
-  for (unsigned i = 0; i < nbIter; ++i)
+  try
     {
+      ODEBUG3("\n1. Construct Grabber\n");
+      // Start with TRIGGER_MODE
+      boost::shared_ptr<ActionGrab> clientGrab(new ActionGrab(true));
+      assert (clientGrab);
+
+      ODEBUG3("\n2. Initialize Grabber\n");
+      clientGrab->Initialize();
       if (Verbose>2)
-	std::cout << "Frame " << i << iendl;
+	std::cout << *clientGrab << std::endl;
+
+      ODEBUG3("\n3. Execute Grabber\n");
       clientGrab->ExecuteAction();
+
+      ODEBUG3("\n4. Construct Display\n");
+      //ActionDisplayMbt display(clientGrab, "ElectricWallFar", "default");
+      ActionDisplayMbt display(clientGrab, "jrlLabGround", "default",
+			       vpColor::blue, true);
+      if (Verbose>2)
+	std::cout << *clientGrab << std::endl;
+
+      display.trackingClient()->setTrackingParameters("DATA", "ON");
+
+
+      ODEBUG3("\n4. Initialize Display\n");
+      display.Initialize();
+
+
+      ODEBUG3("\n4.5 Initialize Display\n");
+      // Switch to FLOW
+      clientGrab->setTriggerMode(false);
+
+      ODEBUG3("\n5. Execute Display\n");
       display.ExecuteAction();
+
+      ODEBUG3("\n6. Print all\n");
       if (Verbose>2)
 	std::cout << *clientGrab << iendl
 		  << display << iendl;
+
+      ODEBUG3("\n7. LOOP\n");
+
+      unsigned int nbIter = 3000000;
+      for (unsigned i = 0; i < nbIter; ++i)
+	{
+	  if (Verbose>2)
+	    std::cout << "Frame " << i << iendl;
+	  clientGrab->ExecuteAction();
+	  display.ExecuteAction();
+	  if (Verbose>2)
+	    std::cout << *clientGrab << iendl
+		      << display << iendl;
+	}
+      display.CleanUp();
+      clientGrab->CleanUp();
+
+    }
+  catch(char *myexception)
+    {
+      std::cerr<< "Exception:" << myexception << std::endl;
     }
 
-  display.CleanUp();
-  clientGrab->CleanUp();
   return 0;
 }
